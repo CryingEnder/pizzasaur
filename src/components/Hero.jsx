a5b8fb1d4c2e673aeee21c386f82b706a43326d3
Cristian Botez <cbotez22@gmail.com>
2022-01-07 16:51:24 +0200

    Build the Welcome component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { HeroPicture } from "./common/Pictures";
 import Container from "./common/Container";
+import Welcome from "./Welcome";
 
 function Hero(props) {
   return (
     <Container
       tag="section"
       stylesOut={"bg-gradient-to-b from-red-darker to-red-dark z-10"}
-      stylesIn={"flex flex-row justify-center items-center"}
+      stylesIn={"flex flex-col justify-center items-center"}
     >
-      <HeroPicture className={"laptop:my-12 laptop:px-2 w-full"} />
+      <HeroPicture
+        className={"laptop:mb-20 laptop:px-2 w-full laptop:order-1"}
+      />
+      <Welcome styles="my-10 tablet:my-16 laptop:mb-14 laptop:mt-10" />
     </Container>
   );
 }
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Welcome({ styles }) {
+  return (
+    <h1
+      className={`text-zinc-100 m-2 text-3xl tablet-small:text-4xl tablet:text-5xl laptop:text-6xl drop-shadow-md desktop:text-7xl text-center underline underline-offset-2 decoration-orange-500 laptop:decoration-yellow-400 ${styles}`}
+    >
+      Welcome to Pizzasaur
+    </h1>
+  );
+}
+
+Welcome.defaultProps = {
+  styles: "",
+};
+
+Welcome.propTypes = {
+  styles: PropTypes.string,
+};
+
+export default Welcome;
