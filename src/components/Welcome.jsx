f61e5647a70da6bcf4342915b1654ec7dca7e593
Cristian Botez <cbotez22@gmail.com>
2022-01-08 17:31:35 +0200

    Change the font of Welcome component

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 function Welcome({ styles }) {
   return (
     <h1
-      className={`text-zinc-100 m-2 text-3xl tablet-small:text-4xl tablet:text-5xl laptop:text-6xl drop-shadow-md desktop:text-7xl text-center underline underline-offset-2 decoration-orange-500 laptop:decoration-yellow-400 ${styles}`}
+      className={`text-zinc-100 m-2 text-3xl tablet-small:text-4xl tablet:text-5xl laptop:text-6xl drop-shadow-md font-heading desktop:text-7xl text-center underline underline-offset-2 decoration-orange-500 laptop:decoration-yellow-400 ${styles}`}
     >
       Welcome to Pizzasaur
     </h1>
