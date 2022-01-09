d9d11a9de0a26af08538a6ec91833796e43661fd
Cristian Botez <cbotez22@gmail.com>
2022-01-09 17:50:13 +0200

    Refactor Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { HeroPicture } from "./common/Pictures";
 import Container from "./common/Container";
-import Welcome from "./Welcome";
 
 function Hero(props) {
   return (
     <Container
-      tag="section"
+      tag="header"
       stylesOut={"bg-gradient-to-b from-red-darker to-red-dark z-10"}
       stylesIn={"flex flex-col justify-center items-center"}
     >
-      <HeroPicture
-        className={"laptop:mb-20 laptop:px-2 w-full laptop:order-1"}
-      />
-      <Welcome styles="my-10 tablet:my-16 laptop:mb-14 laptop:mt-10" />
+      <HeroPicture className="w-full laptop:px-2 laptop:order-1 laptop:mb-24 desktop:mb-28" />
+      <h1 className="text-zinc-100 drop-shadow-md text-center underline underline-offset-2 decoration-orange-500 mx-2 mt-7 mb-14 tablet-small:mt-9 tablet-small:mb-16 tablet:mb-20 laptop:mb-16 laptop:decoration-yellow-400">
+        Welcome to Pizzasaur
+      </h1>
     </Container>
   );
 }
