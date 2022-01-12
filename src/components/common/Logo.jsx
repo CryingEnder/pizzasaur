8e3ee85eaa860acfbeed9e08c58bb3562f7ddb70
Cristian Botez <cbotez22@gmail.com>
2022-01-12 16:34:27 +0200

    Make Logo act as a link to the home page

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { logo } from "../../utils/images";
+import { Link } from "react-router-dom";
 
 function Logo({ ...props }) {
+  function scrollUp() {
+    window.scrollTo(0, 0);
+  }
+
   return (
-    <img
-      {...props}
-      className="w-40 tablet:w-44 laptop:w-56 desktop:w-64 cursor-pointer drop-shadow-lg"
-      src={logo}
-      alt="Pizzasaur logo"
-    />
+    <Link onClick={scrollUp} to="/">
+      <img
+        {...props}
+        className="w-40 tablet:w-44 laptop:w-56 desktop:w-64 cursor-pointer drop-shadow-lg"
+        src={logo}
+        alt="Pizzasaur logo"
+      />
+    </Link>
   );
 }
 
