4bd0264bf3d5b33dd495a5203b1e5b340e160060
Cristian Botez <cbotez22@gmail.com>
2022-02-24 15:13:11 +0200

    Update the CSS of Search for bigger devices

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -14,10 +14,10 @@ function Search({ stylesOut, ...props }) {
     <Container
       tag="div"
       stylesOut={stylesOut}
-      stylesIn="relative flex w-64 flex-row items-center justify-center px-2 pt-8 tablet:mt-1 tablet:w-48 tablet:p-0"
+      stylesIn="relative flex flex-row items-center justify-center px-2 pt-8 tablet:mt-1 tablet:p-0 tablet:mr-3 w-64 tablet:w-48 desktop:w-56"
     >
       <MagnifyingGlass
-        className="absolute left-6 w-5 cursor-text fill-current text-zinc-500 tablet:left-3 tablet:w-4"
+        className="absolute left-6 w-5 cursor-text fill-current text-slate-500 tablet:left-3 tablet:w-4 desktop:left-3.5 desktop:mt-0.5 desktop:w-5"
         onClick={focusInput}
       />
       <input
@@ -25,7 +25,9 @@ function Search({ stylesOut, ...props }) {
         ref={ref}
         type="text"
         placeholder="Quick search..."
-        className="w-full rounded-2xl bg-slate-200 py-4 pl-12 pr-2 font-ui text-xl font-medium text-zinc-800 placeholder-zinc-500 outline-none placeholder:text-xl focus:bg-zinc-200 tablet:py-1 tablet:pl-9 tablet:text-lg tablet:placeholder:text-lg"
+        className="w-full rounded-xl bg-gradient-to-br from-slate-200 via-slate-100 to-slate-200 py-4 pl-12 pr-2
+         font-ui text-xl font-medium text-slate-800 placeholder-slate-500 outline-none placeholder:text-xl tablet:py-1 tablet:pl-9
+          tablet:text-lg tablet:placeholder:text-xl desktop:py-1.5 desktop:pl-11 desktop:text-2xl desktop:placeholder:text-2xl"
       />
     </Container>
   );
